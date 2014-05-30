af4db0b0049ef0349976702b899b71bc1d5d0e42
Drew Kowalik <dk@epantry.com>
2014-05-29 18:48:38 -0700

    update token refresh

--- tradegecko/__init__.py
+++ tradegecko/__init__.py
@@ -13,59 +13,54 @@ def find_credentials():
 
 
 class TradeGeckoRestClient(object):
-    def __init(self):
-        print 'Hello world'
 
-    # def __init__(self, app_id=None, app_secret=None, access_token=None, refresh_token=None):
-    #     if not app_id or app_secret:
-    #         self.app_id, self.app_secret = find_credentials()
-    #     else:
-    #         self.app_id = app_id
-    #         self.app_secret = app_secret
-    #     self.access_token = access_token
-    #     self.refresh_token = refresh_token
-    #
-    #     if not self.app_id or not self.app_secret or not self.access_token:
-    #         #TODO create specific exception
-    #         #TODO refactor
-    #         raise Exception("Auth Error")
-    #
-    #     self.base_uri = 'https://api.tradegecko.com/'
-    #     ##TODO env var
-    #     self.redirect_uri = os.environ['TRADEGECKO_REDIRECT']
-    #
-    #     self.base_data = {
-    #         'client_id': self.app_id,
-    #         'client_secret': self.app_secret,
-    #         'redirect_uri': self.redirect_uri
-    #     }
-    #
-    # def generate_data(self, data):
-    #     #merge dictionary and return json
-    #     return json.dumps(dict(self.base_data.items() + data.items()))
-    #
-    # def send_request(self, method, uri, data):
-    #     data = self.generate_data(data)
-    #     headers = {'content-type': 'application/json'}
-    #     return requests.request(method, uri, data=data, headers=headers)
-    #
-    # def refresh_token(self):
-    #     if not self.refresh_token:
-    #         raise Exception("Missing refresh token")
-    #
-    #     uri = self.base_uri + 'oauth/token'
-    #     data = {
-    #         'refresh_token': self.refresh_token,
-    #         'grant_type': 'refresh_token'
-    #     }
-    #
-    #     rsp = self.send_request('POST', uri, data)
-    #
-    #     if rsp.status_code == 200:
-    #         rsp_data = rsp.json()
-    #         return rsp_data['access_token'], rsp_data['refresh_token']
-    #     else:
-    #         return False
+    def __init__(self, app_id=None, app_secret=None, access_token=None, refresh_token=None):
+        if not app_id or app_secret:
+            self.app_id, self.app_secret = find_credentials()
+        else:
+            self.app_id = app_id
+            self.app_secret = app_secret
+        self.access_token = access_token
+        self.refresh_token = refresh_token
 
+        if not self.app_id or not self.app_secret or not self.access_token:
+            #TODO create specific exception
+            #TODO refactor
+            raise Exception("Auth Error")
 
+        self.base_uri = 'https://api.tradegecko.com/'
+        ##TODO env var
+        self.redirect_uri = os.environ['TRADEGECKO_REDIRECT']
 
+        self.base_data = {
+            'client_id': self.app_id,
+            'client_secret': self.app_secret,
+            'redirect_uri': self.redirect_uri
+        }
+
+    def generate_data(self, data):
+        #merge dictionary and return json
+        return json.dumps(dict(self.base_data.items() + data.items()))
+
+    def send_request(self, method, uri, data):
+        data = self.generate_data(data)
+        headers = {'content-type': 'application/json'}
+        return requests.request(method, uri, data=data, headers=headers)
+
+    def get_refresh_token(self):
+        if not self.refresh_token:
+            raise Exception("Missing refresh token")
+
+        uri = self.base_uri + 'oauth/token'
+        data = {
+            'refresh_token': self.refresh_token,
+            'grant_type': 'refresh_token'
+        }
+
+        rsp = self.send_request('POST', uri, data)
+
+        if rsp.status_code == 200:
+            rsp_data = rsp.json()
+            return rsp_data['access_token'], rsp_data['refresh_token']
+        else:
+            return False, False
