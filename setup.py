68aa31ce75ad92692407f034ab213410678404cf
Anton Shutik <anton.shutik@itechart-group.com>
2015-05-18 15:58:50 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'tradegecko-python',
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'Python wrapper for TradeGecko REST API',
     author = 'Drew Kowalik',
     author_email = 'dk@epantry.com',
@@ -12,4 +12,4 @@ setup(
     install_requires = ['requests==2.3.0'],
     packages = find_packages(),
     include_package_data=True,
-    )
+)
