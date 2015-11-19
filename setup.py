dc4dd2a2bf1cdf1254a19065dcb0f65325c782a9
chrisclark <chris@untrod.com>
2015-11-19 14:57:00 -0800

    Prep for upcoming release 0.0.7

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'tradegecko-python',
-    version = '0.0.6',
+    version = '0.0.7',
     description = 'Python wrapper for TradeGecko REST API',
-    author = 'Drew Kowalik',
-    author_email = 'dk@epantry.com',
+    author = 'Drew Kowalik, Chris Clark',
+    author_email = 'team@epantry.com',
     url = 'https://github.com/epantry/tradegecko-python',
     keywords = ['tradegecko'],
     install_requires = ['requests==2.3.0'],
